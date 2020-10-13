99ff4fda146023e2ce4512b6b259694ed249665e
MarekPadd13 <markpdd13@list.ru>
2020-10-13 22:38:15 +0300

    'seul-fixs'

--- views/house-image/partials/_angles.php
+++ views/house-image/partials/_angles.php
@@ -3,10 +3,10 @@
 <div class="row">
     <div class="col-md-12">
         <h3><?= $angle->angle->name  ?></h3>
-        `<?= $this->render('_angle', [
+        <?= $this->render('_angle', [
             'model' => $model,
             'angle_id' => $angle->angle_id
-        ]) ?>`
+        ]) ?>
     </div>
 </div>
 <?php endforeach; ?>
