faf7185f88680c32d65a45a9147ac5b7fffc40a0
MarekPadd13 <markpdd13@list.ru>
2020-11-22 18:54:59 +0300

    fix

--- views/house-image/partials/_angles.php
+++ views/house-image/partials/_angles.php
@@ -1,13 +1,16 @@
 
 <?php use yii\helpers\Html;
 
-foreach ($model->angleGroup as $index => $angle): $homeImage = $model->getAnglesImage($angle->angle_id) ?>
+foreach ($model->angleGroup as $index => $angle):
+    $homeImage = $model->getAnglesImage($angle->angle_id);
+$text = "Дом №". $model->name .". Ракурс ". $angle->angle->name.". "; ?>
 <div class="row">
     <div class="col-md-12">
         <h4>
             <span style="color: <?= $homeImage && $homeImage->published ? "green" : "black"?>"><?= $angle->angle->name . ($homeImage ? ".  Дата последнего снимка: " . $homeImage->dateView : "") ?></span>
             <?= $homeImage ? Html::a("Загрузить фото", ['add-photo-home-and-angle', 'home_id'=> $model->id, 'angle_id'=> $angle->angle_id, ],
                 [ 'class'=> 'btn btn-link']) : "" ?>
+            <a class= 'btn btn-link' href="https://api.whatsapp.com/send?phone=79670262728&text=<?= $text ?>">Нашли изменения?</a>
         </h4>
         <?php if($homeImage && $homeImage->published) : ?>
             <p>Есть изменения: <?= $homeImage->description ?></p>
--- views/house-image/partials/_buttons_angle.php
+++ views/house-image/partials/_buttons_angle.php
@@ -9,11 +9,12 @@ $array = []
 
 <?php if($array && key_exists($angle_id, $array)) :
    $homeImage = $model->getAnglesImage($angle_id);
-    ?>
+    $text = "Дом №". $model->name .". Ракурс ". $array[$angle_id].". " ?>
     <h4>
         <span style="color: <?= $homeImage && $homeImage->published ? "green" : "black"?>"><?=$array[$angle_id] . ($homeImage ? ".  Дата последнего снимка: " . $homeImage->dateView : "") ?></span>
         <?= $homeImage  ? Html::a("Загрузить фото", ['add-photo-home-and-angle', 'home_id'=> $model->id, 'angle_id'=> $angle_id, ],
             ['class'=> 'btn btn-link']) : '' ?>
+        <a class= 'btn btn-link' href="https://api.whatsapp.com/send?phone=79670262728&text=<?= $text ?>">Нашли изменения?</a>
     </h4>
     <?php if($homeImage && $homeImage->published) : ?>
     <p>Есть изменения: <?= $homeImage->description ?></p>
